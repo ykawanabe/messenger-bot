979d2a2513bec536224bb5ba50bf500fa44d59a9
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2019-01-06 23:14:59 -0800

    Reply feature

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 import os
 from flask import Flask, request
-from fbmessenger import BaseMessenger
-
-class Messenger(BaseMessenger):
-    def __init__(self, page_access_token):
-        self.page_access_token = page_access_token
-        super(Messenger, self).__init__(self.page_access_token)
-
-    def message(self, message):
-        res = self.send({'text': str(message['message']['text'])})
-        app.logger.debug('Response: {}'.format(res))
-
-    def delivery(self, message):
-        pass
-
-    def read(self, message):
-        pass
-
-    def account_linking(self, message):
-        pass
-
-    def postback(self, message):
-        pass
-
-    def optin(self, message):
-        pass
+from pymessenger.bot import Bot
 
 app = Flask(__name__)
 app.debug = True
-messenger = Messenger(os.environ.get('FB_PAGE_TOKEN'))
+ACCESS_TOKEN = os.environ.get('FB_ACCESS_TOKEN')
+bot = Bot(ACCESS_TOKEN)
 
 @app.route("/webhook", methods=['GET','POST'])
 def webhook():
-    print("Webhook")
+    print('hook!')
     if request.method == 'GET':
-        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
-            print("Here!")
-            return request.args.get('hub.challenge')
-        raise ValueError('FB_VERIFY_TOKEN does not match.')
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
     elif request.method == 'POST':
-        messenger.handle(request.get_json(force=True))
-    return ''
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for m in messaging:
+                if m.get('message'):
+                    recipient_id = m['sender']['id']
+                    bot.send_action(recipient_id, 'mark_seen')
+                    if m['message'].get('text'):
+                        response = m['message']['text']
+                        send_message(recipient_id, response)
+                    if m['message'].get('attachments'):
+                        for att in m['message'].get('attachments'):
+                            response = recipient_id, att['type'], att['payload']['url']
+                            send_message(recipient_id, response)
+                else:
+                    pass
+    return "Message Processed"
+
+# def handle_post(json):
+#     for event in output
+
+def verify_fb_token(token_sent):
+    verify_token = os.environ.get('FB_VERIFY_TOKEN')
+    if token_sent == verify_token:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+    return "success"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
